23d76c7f67790eba4bccfc0c1691048181102f70
Jared Keffer <jaredkeffer@Jareds-MacBook-Pro.local>
2019-08-16 14:55:49 -0600

    added columns, working on snap functionality

---
+++ src/todo.js
@@ -0,0 +1,91 @@
+newNodeButton = document.getElementById("NewNode");
+newNodeButton.addEventListener("mousedown", CreateCard);
+
+var container = document.querySelector("#container");
+var theFirstChild = container.firstChild;
+
+var cards = [];
+var nodes = [];
+var snapping = true;
+
+function CreateCard(e) {
+	var newCard = document.createElement("div");
+	newCard.classList.add("card");
+
+	newCard.addEventListener("mousedown", dragStart, false);
+	newCard.addEventListener("mousemove", drag, false);
+	newCard.addEventListener("mouseleave", deactivate, false);
+	newCard.addEventListener("mouseup", dragEnd, false);
+	
+	
+	var textCard = document.createElement("p");
+	var newContent = document.createTextNode("");
+	textCard.appendChild(newContent);
+	newCard.appendChild(textCard);
+
+	
+
+	container.insertBefore(newCard, theFirstChild);
+	newCard.classList.add('active');
+
+	cards.unshift(newCard);
+
+	var active = true;
+	var currentX = 0;
+	var currentY = 0;
+	var offsetX = 0;
+	var offsetY = 0;
+	var initialX = e.clientX - offsetX;;
+	var initialY = e.clientY - offsetY;;
+	
+
+
+	function dragStart(e) {	
+		initialX = e.clientX - offsetX;
+		initialY = e.clientY - offsetY;
+		if (e.target === newCard) {
+			e.preventDefault();
+			newCard.classList.remove('inactive');
+			newCard.classList.add('active');
+			active = true;
+		}
+	}
+	function dragEnd(e) {
+		newCard.classList.remove('active');
+		newCard.classList.add('inactive');
+		initialX = currentX;
+		initialY = currentY;
+		active = false;
+		if (snapping) {
+			snap(newCard);
+		}
+	}
+	function deactivate(e) {
+		if (active) {
+			newCard.classList.remove('active');
+			newCard.classList.add('inactive');
+			active = false;
+			if (snapping) {
+				snap(newCard);
+			}
+		}
+	}
+	function drag(e) {
+		if (active && document.hasFocus()) {
+			// e.preventDefault();
+			currentX = e.clientX - initialX;
+			currentY = e.clientY - initialY;
+			offsetX = currentX;
+			offsetY = currentY;
+			setTranslate(currentX, currentY, newCard);
+		}
+	}
+	function setTranslate(xPos, yPos, el) {
+		el.style.transform = "translate(" + xPos + "px, " + yPos + "px)";
+	}
+}
+function snap(el) {
+	console.log("bike");
+	el.closest(".node");
+	console.log(el.closest(".node"));
+}
