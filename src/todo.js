9ae664d722d2e73e9a4af8e57a7c303f5b469d3e
Jared Keffer <jaredkeffer@Jareds-MacBook-Pro.local>
2019-08-16 19:11:12 -0600

    create snapping grid

--- src/todo.js
+++ src/todo.js
@@ -6,7 +6,7 @@ var theFirstChild = container.firstChild;
 
 var cards = [];
 
-var snapping = true;
+var snapping = false;
 
 function CreateCard(e) {
 	var newCard = document.createElement("div");
@@ -23,97 +23,108 @@ function CreateCard(e) {
 	textCard.appendChild(newContent);
 	newCard.appendChild(textCard);
 
-	
-
 	container.insertBefore(newCard, theFirstChild);
 	newCard.classList.add('active');
 
 	cards.unshift(newCard);
 
 	var active = true;
-	var currentX = 0;
-	var currentY = 0;
-	var offsetX = 0;
-	var offsetY = 0;
-	var initialX = e.clientX - offsetX;;
-	var initialY = e.clientY - offsetY;;
+	var offset = 75;
 	
-
-
 	function dragStart(e) {	
-		initialX = e.clientX - offsetX;
-		initialY = e.clientY - offsetY;
+		snapping = false;
+		
 		if (e.target === newCard) {
-			e.preventDefault();
+			var cardRect = newCard.getBoundingClientRect();
+			var x = parseInt(cardRect.left);
+			var y = parseInt(cardRect.top);
+			var oldNode = getClosestNode(x, y);
+			console.log(oldNode);
+			oldNode.classList.add("activeNode");
 			newCard.classList.remove('inactive');
 			newCard.classList.add('active');
 			active = true;
 		}
 	}
 	function dragEnd(e) {
+		snapping = true;
 		newCard.classList.remove('active');
 		newCard.classList.add('inactive');
-		initialX = currentX;
-		initialY = currentY;
 		active = false;
 		if (snapping) {
-			snap(newCard);
+			console.log("bike");
+			snap(newCard, e.clientX, e.clientY, snapping, e);
 		}
 	}
 	function deactivate(e) {
 		if (active) {
+			snapping = true;
 			newCard.classList.remove('active');
 			newCard.classList.add('inactive');
 			active = false;
 			if (snapping) {
-				snap(newCard);
+				snap(newCard, e.clientX, e.clientY, snapping, e);
 			}
 		}
 	}
 	function drag(e) {
-		if (active && document.hasFocus()) {
-			// e.preventDefault();
-			currentX = e.clientX - initialX;
-			currentY = e.clientY - initialY;
-			offsetX = currentX;
-			offsetY = currentY;
-			setTranslate(currentX, currentY, newCard);
+		if (active) {
+			e.preventDefault();
+			snapping = false;
+			setTranslate(e.clientX - offset, e.clientY - offset, newCard);
 		}
 	}
 	function setTranslate(xPos, yPos, el) {
 		el.style.transform = "translate(" + xPos + "px, " + yPos + "px)";
 	}
+	function snap(el, xPos, yPos, snapping, e) {
+		if (snapping) {	
+			var closestNode = getClosestActiveNode(xPos, yPos);
+			closestNode.classList.remove("activeNode");
+			var nodeRect = closestNode.getBoundingClientRect();
+			var x = parseInt(nodeRect.left);
+			var y = parseInt(nodeRect.top);
+
+			setTranslate(x, y, el);
+		}
+	}
 }
-function snap(el) {
-	console.log("bike");
-	console.log(getPosition(el));
-}
 
+function getClosestNode(x, y) {
+	let nodes = Array.from(document.querySelectorAll(".node"));
+	let nodeCoords = nodes.map(link => {
+		let rect = link.getBoundingClientRect();
+	return [rect.x, rect.y];
+	});
 
-
-
-let nodes = Array.from(document.querySelectorAll(".node"));
-console.log(nodes);
-
-// let elements = Array.from(document.querySelectorAll('a'));
-
-let nodeCoords = nodes.map(link => {
-	let rect = link.getBoundingClientRect();
-  return [rect.x, rect.y];
-});
-
-
-document.addEventListener("click", ev => {
 	let distances = [];
 
-  nodeCoords.forEach(nodeSpot => {
-	  let distance = 0;
-	  distance = Math.hypot(nodeSpot[0]-parseInt(ev.clientX), nodeSpot[1]-parseInt(ev.clientY));
-    distances.push(parseInt(distance));
-  });
+	nodeCoords.forEach(nodeSpot => {
+		let distance = 0;
+		distance = Math.hypot(nodeSpot[0]-parseInt(x), nodeSpot[1]-parseInt(y));
+		distances.push(parseInt(distance));
+	});
 
-  let closestNodeIndex = distances.indexOf(Math.min(...distances));
+	let closestNodeIndex = distances.indexOf(Math.min(...distances));
 
-//   document.getElementById('result').innerHTML = (elements[closestNodeIndex].id);
-	console.log(nodes[closestNodeIndex]);
-});
+	return nodes[closestNodeIndex];
+}
+function getClosestActiveNode(x, y) {
+	let nodes = Array.from(document.querySelectorAll(".activeNode"));
+	let nodeCoords = nodes.map(link => {
+		let rect = link.getBoundingClientRect();
+	return [rect.x, rect.y];
+	});
+
+	let distances = [];
+
+	nodeCoords.forEach(nodeSpot => {
+		let distance = 0;
+		distance = Math.hypot(nodeSpot[0]-parseInt(x), nodeSpot[1]-parseInt(y));
+		distances.push(parseInt(distance));
+	});
+
+	let closestNodeIndex = distances.indexOf(Math.min(...distances));
+
+	return nodes[closestNodeIndex];
+}
